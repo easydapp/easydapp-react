a6d72d4d357e75053a0b6307838f337178e96114
Monte9567 <monte9567@users.noreply.github.com>
2025-03-21 17:00:00 +0800

    pref: fetch dapp

--- packages/combined/fetch.ts
+++ packages/combined/fetch.ts
@@ -17,51 +17,43 @@ export const fetch_data = async (
     upper_query_api?: (anchor: ApiDataAnchor) => Promise<ApiData | undefined>,
     upper_query_combined?: (anchor: CombinedAnchor) => Promise<Combined | undefined>,
 ): Promise<
-    [
-        Combined,
-        Publisher,
-        [CodeDataAnchor, CodeData][],
-        [ApiDataAnchor, ApiData][],
-        [CombinedAnchor, Combined][],
-    ]
+    [Combined, Publisher, [CodeDataAnchor, CodeData][], [ApiDataAnchor, ApiData][], [CombinedAnchor, Combined][]]
 > => {
-    return Promise.all([
-        (upper_query_combined ?? proxy_query_combined)(dapp.combined).then((combined) => {
-            if (!combined) throw new Error(`combined not found: ${dapp.combined}`);
-            return combined;
-        }),
-        (upper_query_publisher ?? proxy_query_publisher)(dapp.publisher).then((user) => {
-            if (!user) throw new Error(`user not found: ${dapp.publisher}`);
-            return user;
-        }),
-        Promise.all(
-            (dapp.metadata?.code_anchors ?? []).map(async (code_anchor) => {
-                return (upper_query_code ?? proxy_query_code)(code_anchor).then((code) => {
-                    if (!code) throw new Error(`code not found: ${code_anchor}`);
-                    const result: [CodeDataAnchor, CodeData] = [code_anchor, code];
-                    return result;
-                });
-            }),
-        ),
-        Promise.all(
-            (dapp.metadata?.apis_anchors ?? []).map(async (apis_anchor) => {
-                return (upper_query_api ?? proxy_query_api)(apis_anchor).then((api) => {
-                    if (!api) throw new Error(`api not found: ${apis_anchor}`);
-                    const result: [ApiDataAnchor, ApiData] = [apis_anchor, api];
-                    return result;
-                });
-            }),
-        ),
-        Promise.all(
-            (dapp.metadata?.combined_anchors ?? []).map(async (combined_anchor) => {
-                return (upper_query_combined ?? proxy_query_combined)(combined_anchor).then(
-                    (combined) => {
-                        if (!combined) throw new Error(`combined not found: ${combined_anchor}`);
-                        const result: [CombinedAnchor, Combined] = [combined_anchor, combined];
-                        return result;
-                    },
-                );
-            }),
-        ),
-    ]);
+    const combined_promise = (upper_query_combined ?? proxy_query_combined)(dapp.combined).then((combined) => {
+        if (!combined) throw new Error(`combined not found: ${dapp.combined}`);
+        return combined;
+    });
+    const publisher_promise = (upper_query_publisher ?? proxy_query_publisher)(dapp.publisher).then((user) => {
+        if (!user) throw new Error(`user not found: ${dapp.publisher}`);
+        return user;
+    });
+    const code_promises = (dapp.metadata?.code_anchors ?? []).map(async (code_anchor) => {
+        return (upper_query_code ?? proxy_query_code)(code_anchor).then((code) => {
+            if (!code) throw new Error(`code not found: ${code_anchor}`);
+            const result: [CodeDataAnchor, CodeData] = [code_anchor, code];
+            return result;
+        });
+    });
+    const apis_promises = (dapp.metadata?.apis_anchors ?? []).map(async (apis_anchor) => {
+        return (upper_query_api ?? proxy_query_api)(apis_anchor).then((api) => {
+            if (!api) throw new Error(`api not found: ${apis_anchor}`);
+            const result: [ApiDataAnchor, ApiData] = [apis_anchor, api];
+            return result;
+        });
+    });
+    const dapp_promises = (dapp.metadata?.combined_anchors ?? []).map(async (combined_anchor) => {
+        return (upper_query_combined ?? proxy_query_combined)(combined_anchor).then((combined) => {
+            if (!combined) throw new Error(`combined not found: ${combined_anchor}`);
+            const result: [CombinedAnchor, Combined] = [combined_anchor, combined];
+            return result;
+        });
+    });
+
+    return [
+        await combined_promise,
+        await publisher_promise,
+        await Promise.all(code_promises),
+        await Promise.all(apis_promises),
+        await Promise.all(dapp_promises),
+    ];
 };
